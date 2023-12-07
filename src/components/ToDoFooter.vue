49c7c78637eb0f7fbaede59fdc341865b9f519ef
dmystical-coder <dmystical.coder@gmail.com>
2023-12-07 08:07:25 +0100

    Compile and minnify for production

--- src/components/ToDoFooter.vue
+++ src/components/ToDoFooter.vue
@@ -1,9 +1,3 @@
-<!-- TodoClearButton -->
-<!-- clear completed => clear all -->
-
-<!-- Todo Statistics -->
-<!-- pending -->
-
 <template>
   <div id="alert-border-1" role="alert" class="my-alert">
     <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 26 26" fill="none">
